87ec6871276b3a4ee2cabc32f6946ecc0671b16e
Marian <marianlachu2002@gmail.com>
2024-12-04 14:12:46 +0530

    second commit

--- LibraryManagementSystem/lmsapp/models.py
+++ LibraryManagementSystem/lmsapp/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title=models.CharField(max_length=255)
-    author=models.CharField(max_length=255)
-    genre=models.CharField(max_length=255)
-    price=models.DecimalField()
-    
+# class Book(models.Model):
+#     title=models.CharField(max_length=255)
+#     author=models.CharField(max_length=255)
+#     genre=models.CharField(max_length=255)
+#     price=models.DecimalField()
