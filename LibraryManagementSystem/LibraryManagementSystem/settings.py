ed1c6096f18b10e47e2b25cdfb2fbe0613cc47e1
Marian <marianlachu2002@gmail.com>
2024-12-04 13:04:32 +0530

    first commit

--- LibraryManagementSystem/LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/LibraryManagementSystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'lmsapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'LibraryManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library_db',  
+        'USER': 'root',  
+        'PASSWORD': 'Marian@2002',  
+        'HOST': 'localhost',  
+        'PORT': '3306' 
     }
 }
 
