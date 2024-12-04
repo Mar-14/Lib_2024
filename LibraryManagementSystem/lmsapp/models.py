c1e52aaeb3d70f4579822188d6f0f9b8619dcddf
Marian <marianlachu2002@gmail.com>
2024-12-04 21:46:35 +0530

    changes added

--- LibraryManagementSystem/lmsapp/models.py
+++ LibraryManagementSystem/lmsapp/models.py
@@ -1,8 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-# class Book(models.Model):
-#     title=models.CharField(max_length=255)
-#     author=models.CharField(max_length=255)
-#     genre=models.CharField(max_length=255)
-#     price=models.DecimalField()
+class Author(models.Model):
+    author_name = models.CharField(max_length=100)
+    bio = models.TextField(null=True, blank=True)
+ 
+    def __str__(self):
+        return f"{self.name}"
+    
+class category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title=models.CharField(max_length=255)
+    author=models.CharField(max_length=255)
+    genre=models.CharField(max_length=100)
+    category=models.ForeignKey(category, on_delete=models.CASCADE)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+    available_copies = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Membership(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plan_name = models.CharField(max_length=50)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.plan_name} Plan"
--- LibraryManagementSystem/lmsapp/views.py
+++ LibraryManagementSystem/lmsapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def user_login(request):
+#     if request.method=='POST':
+#         #we will be getting username and password through POST
+#         login_form=MyLoginForm(request.POST)
+#         if login_form.is_valid():
+#             clesned_data=login_form.cleaned_data
+#             auth_user=authenticate(request,username=clesned_data['username'],password=clesned_data['password'])
+#             if auth_user is not None:
+#                 login(request,auth_user)
+#                 #get the user's group name
+#                 group=auth_user.groups.first() #assumes a user has
+#                 group_name =group.name if group else "No Group"
+#                 request.session['group_name'] = group_name
+#                 return redirect('home_path')
+#             else:
+#                 return HttpResponse('Not Authenticated')
+#     else:
+#         login_form=MyLoginForm()
+#     return render(request,'useraccount/login_form.html',{'login_form':login_form})
