64028b32635e3efa1fdb5f88df9c83b83dc079db
Marian <marianlachu2002@gmail.com>
2024-12-05 11:08:16 +0530

    2

--- LibraryManagementSystem/lmsapp/admin.py
+++ LibraryManagementSystem/lmsapp/admin.py
@@ -3,7 +3,3 @@ from django.contrib import admin
 from .models import Author,category,Book,Membership
 
 # Register your models here.
-admin.site.register(Author) 
-admin.site.register(category) 
-admin.site.register(Book) 
-admin.site.register(Membership) 
